195cabf0756fed43daf9b921940fb6cbdc72b092
Mathersg4 <156609158+Mathersg4@users.noreply.github.com>
2025-05-13 21:49:59 +0300

    Create mm2tradehelper.lua

---
+++ mm2tradehelper.lua
@@ -0,0 +1,7 @@
+MinimumProfitPercentage = 6 -- for auto offer calculations!
+AutoTrade = false -- auto offer itself
+-- Menu trade
+loadstring(game:HttpGet("https://raw.githubusercontent.com/f4shn/X/refs/heads/main/mm2-trade-helper.lua",true))()
+
+-- Value Items
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mathersg4/software/refs/heads/main/pr.lua"))()
